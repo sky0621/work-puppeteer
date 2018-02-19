cd1cd3f5cdb53990e18cc5c34d76d3d695fec9cc
sky0621 <sky14work@gmail.com>
2018-02-20 02:29:53 +0900

    puppeteer

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://developers.line.me/ja/docs/messaging-api/reference/', {waitUntil: 'networkidle', networkIdleTimeout: 5000});
-    await page.screenshot({path: 'developers.line.me.messaging-api.001.png', fullPage: true});
-    await page.pdf({path: 'developers.line.me.messaging-api.001.pdf', format: 'A4'});
-
-    await browser.close();
-})
+puppeteer.launch().then(
+    async browser => {
+        const page = await browser.newPage();
+        await page.goto('https://github.com/GoogleChrome/puppeteer/', {waitUntil: 'networkidle2'});
+        await page.screenshot({path: 'puppeteer.png', fullPage: true});
+        await browser.close();
+    }
+);
